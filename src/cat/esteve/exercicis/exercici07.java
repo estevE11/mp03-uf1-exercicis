c8f438dcb395adb7eac90394585132569384ed5e
Roger Esteve <Coconaut7@gmail.com>
2019-10-17 23:45:50 +0200

    ex7 fet

--- src/cat/esteve/exercicis/exercici07.java
+++ src/cat/esteve/exercicis/exercici07.java
@@ -1,9 +1,20 @@
 package cat.esteve.exercicis;
 
+import java.util.Scanner;
+
 public class exercici07 {
     // Intercanviar els valors de dues variables prèviament introduïdes
 
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        System.out.print("< A: ");
+        int a = sc.nextInt();
+        System.out.print("< B: ");
+        int b = sc.nextInt();
+        int aa = a;
+        a = b;
+        b = aa;
+        System.out.println("> A: " + a);
+        System.out.println("> B: " + b);
     }
 }
